81fc44f4b5147b95a5a5e7148e475ba8680f962f
adityaa72 <adityah1357908642@gmail.com>
2025-05-14 15:26:54 +0530

    feat: ui 0.1.9

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ function App() {
   return (
     <div className="flex flex-col justify-center items-center py-24 gap-4">
       <Text as="h1" variant="h1">
-        Hello world!
+        Hello world2!
       </Text>
-      <Button>Click Me!</Button>
+      <div></div>
+      <div className="bg-content1"></div>
+      <Button>Hey there i am using Neovim2</Button>
+      <Text as="p" className="text-foreground-muted">
+        HI there
+      </Text>
       <Link href="#">Hii There</Link>
       <Menu trigger={<Button>Trigger</Button>}>
         <MenuItem>Menu Item1</MenuItem>
